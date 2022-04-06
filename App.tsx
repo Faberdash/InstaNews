5b43df09c0e3c594270b77c480b542615ec899a9
Mo'men Alaswad <faberdash55@gmail.com>
2022-04-06 03:57:21 +0200

    feat(api): fetch latest news from news api on load

--- App.tsx
+++ App.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
+import axios from 'axios';
 
 const App: React.FC = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    async function getArticles() {
+      try {
+        await axios
+          .get('https://newsapi.org/v2/top-headlines', {
+            params: {
+              country: 'us',
+              apikey: 'fe7b7fb5b50a47819d4720f613f9d963',
+            },
+          })
+          .then(response => {
+            setArticles(response.data);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getArticles();
+  }, []);
+
   return (
     <View>
       <Text>Hello Typescript!</Text>
